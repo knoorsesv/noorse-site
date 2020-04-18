b0c9a1355d67810a1e9085d9dbde3a1a6689e891
gvdp <vdputteglenn@gmail.com>
2020-04-18 17:03:03 +0200

    Remove css from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,20 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`noorse`]} />
+    <SEO title="Home" keywords={[`noorse`]}/>
     <div>carrousel</div>
-    <div className="columns is-mobile">
-      <div className="column">
+    <div>
+      <div>
         <div>Whute</div>
-        <div header="Trooper"></div>
+        <div>Trooper</div>
       </div>
-      <div className="column">
-        <div header="Nieuws"></div>
-        <Link to="senioren">Senioren</Link>
+      <div>
+        <h1>Nieuws</h1>
       </div>
     </div>
-    <div style={{ height: '500px' }}>padding</div>
   </Layout>
 )
 
