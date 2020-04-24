e9e200668932dfd3368f5de6621867f7be1d707b
gvdp <vdputteglenn@gmail.com>
2020-04-25 01:22:17 +0200

    Info page missed

---
+++ src/pages/info.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const GeschiedenisPage = () => (
+  <Layout>
+    <div>GeschiedenisPage</div>
+  </Layout>
+)
+
+export default GeschiedenisPage
