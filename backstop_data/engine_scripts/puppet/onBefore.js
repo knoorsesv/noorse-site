5c07818e6aead6a8053c4a3b4433cc0c0ba59abe
gvdp <vdputteglenn@gmail.com>
2020-05-02 18:14:07 +0200

    Different url on CI

--- backstop_data/engine_scripts/puppet/onBefore.js
+++ backstop_data/engine_scripts/puppet/onBefore.js
@@ -1,2 +1,7 @@
 module.exports = async (page, scenario, vp) => {
-};
+  console.log(scenario)
+  if (process.env.CI) {
+    scenario.url = scenario.url.replace('host.docker.internal', 'localhost')
+  }
+  console.log(scenario)
+}
