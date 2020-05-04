8be10ec6e683db9dad2ccd3def2c25914b6d5883
gvdp <vdputteglenn@gmail.com>
2020-05-04 20:47:23 +0200

    Add role and keydown handler for newscard a11y

--- src/components/newsCard.js
+++ src/components/newsCard.js
@@ -6,10 +6,19 @@ export const NewsCard = (newsNode) => {
   const goToNews = () => {
     navigate(`/nieuws/${newsNode.title}`)
   }
+
+  const keyDownHandler = (event) => {
+    if (event.key === 'Enter') {
+      goToNews()
+    }
+  }
   return (
     <div
       className={'card mx-2 my-2 max-w-full'}
       onClick={goToNews}
+      onKeyDown={keyDownHandler}
+      role="link"
+      tabIndex="0"
       key={newsNode.title}
     >
       {newsNode.title.includes('spelers') && (
