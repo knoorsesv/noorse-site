9b9f9629dd0f86ea3c8fc9c24c7c8008f1e58ed5
gvdp <vdputteglenn@gmail.com>
2020-05-09 17:42:50 +0200

    Implement Documents page

--- src/pages/documenten.js
+++ src/pages/documenten.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Title } from '../components/titles'
+import { graphql, useStaticQuery } from 'gatsby'
 
-const DocumentenPage = () => (
-  <Layout>
-    <div>Documenten</div>
-  </Layout>
-)
+const DocumentenPage = () => {
+  const documents = useStaticQuery(graphql`
+    query {
+      allContentfulDocument {
+        nodes {
+          naam
+          document {
+            localFile {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Title>Documenten</Title>
+      <div>
+        {documents.allContentfulDocument.nodes.map((documentNode) => {
+          return (
+            <a href={documentNode.document.localFile.publicURL} download>
+              {documentNode.naam}
+            </a>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
 
 export default DocumentenPage
