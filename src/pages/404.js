2da4878df4ca939163b76ef09cb574eac0f1cb30
gvdp <vdputteglenn@gmail.com>
2020-05-12 21:44:38 +0200

    Customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import React from 'react'
-
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { TextBlock } from '../components/text'
+import { Title } from '../components/titles'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesnt exist... the sadness.</p>
+    <Title>Not found</Title>
+    <TextBlock>
+      De pagina die je zoekt bestaat niet.
+      <p>
+        <Link to={'/'}>Go Home</Link>
+      </p>
+    </TextBlock>
   </Layout>
 )
 
