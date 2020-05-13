0407590dd7f5f4213a918e430188b637eedb4c29
gvdp <vdputteglenn@gmail.com>
2020-05-13 21:23:18 +0200

    Add outcome of games and series info

--- src/templates/ploeg-template.js
+++ src/templates/ploeg-template.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import { SubTitle, Title } from '../components/titles'
 import { graphql, Link } from 'gatsby'
+import { ExternalLink } from '../components/text'
 
 export const query = graphql`
   query($teamId: ID!) {
@@ -13,6 +14,17 @@ export const query = graphql`
           name
         }
         awayTeam {
+          name
+        }
+        outcome {
+          status
+          homeTeamGoals
+          awayTeamGoals
+        }
+      }
+      teamSeriesAndRankings(teamId: $teamId, language: nl) {
+        series {
+          serieId
           name
         }
       }
@@ -41,15 +53,26 @@ export default ({ pageContext: { vvInfo, contentfulPloeg }, data }) => {
         <div className={'col-span-1 flex flex-col'}>
           <SubTitle>Info</SubTitle>
           <div className={'flex flex-col'}>
-            <div>Coach</div>
+            <h3 className={'uppercase font-bold'}>Coach</h3>
             {contentfulPloeg.coach &&
               contentfulPloeg.coach.map((coach) => (
                 <span key={coach}>{coach}</span>
               ))}
-            <div>Training</div>
+            <h3 className={'uppercase font-bold'}>Training</h3>
             {contentfulPloeg.training &&
               contentfulPloeg.training.map((training) => (
                 <span key={training}> {training}</span>
+              ))}
+            <h3 className={'uppercase font-bold'}>Reeks</h3>
+            {data.vv &&
+              data.vv.teamSeriesAndRankings.series.map((series) => (
+                <ExternalLink
+                  key={series.name}
+                  url={`https://www.voetbalvlaanderen.be/competitie/${series.serieId}/rangschikking`}
+                >
+                  {' '}
+                  {series.name}
+                </ExternalLink>
               ))}
           </div>
         </div>
@@ -59,8 +82,11 @@ export default ({ pageContext: { vvInfo, contentfulPloeg }, data }) => {
             data.vv.teamCalendar.map((game) => {
               return (
                 <div key={game.id}>
-                  {game.startDate} / {game.homeTeam.name} --{' '}
+                  {game.startDate} /{game.homeTeam.name} --
                   {game.awayTeam.name}
+                  {game.outcome.status === 'finished'
+                    ? `  //  ${game.outcome.homeTeamGoals} -   ${game.outcome.awayTeamGoals}`
+                    : '-'}
                 </div>
               )
             })}
