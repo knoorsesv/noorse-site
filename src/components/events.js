0f7333407bfbd3355298d43ca03a1112c69339e6
gvdp <vdputteglenn@gmail.com>
2020-07-07 22:27:27 +0200

    fix(dates): only use moment to filter and sort, not display

--- src/components/events.js
+++ src/components/events.js
@@ -2,17 +2,9 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import * as moment from 'moment'
 
-const datumToMoment = (event) => {
-  event.datum = moment(event.datum)
-  return event
-}
-
-const eventIsInfuture = (event) => event.datum.isSameOrAfter(moment())
-const earliestFirst = (a, b) => (a.datum.isAfter(b.datum) ? 1 : -1)
-const formatDate = (event) => {
-  event.datum = event.datum.format('DD-MM-YYYY')
-  return event
-}
+const eventIsInfuture = (event) => moment(event.datum).isSameOrAfter(moment())
+const earliestFirst = (a, b) =>
+  moment(a.datum).isAfter(moment(b.datum)) ? 1 : -1
 
 export const EventList = () => {
   const events = useStaticQuery(graphql`
@@ -27,10 +19,8 @@ export const EventList = () => {
   `)
 
   const formattedEvents = events.allContentfulEvenement.nodes
-    .map(datumToMoment)
     .filter(eventIsInfuture)
     .sort(earliestFirst)
-    .map(formatDate)
 
   return (
     <div className={'flex flex-col'}>
