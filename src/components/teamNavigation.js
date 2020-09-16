3558bb9c854e973d23bec6c4cdedb0ba4172be0d
gvdp <vdputteglenn@gmail.com>
2020-09-16 22:49:00 +0200

    fix: add nullcheck

--- src/components/teamNavigation.js
+++ src/components/teamNavigation.js
@@ -8,16 +8,18 @@ export const CategoryTeamNavigation = ({ category, header }) => {
         {header || category.naam}
       </h3>
       <div className={'flex flex-wrap justify-around mx-1'}>
-        {category.ploeg.map((ploeg) => (
-          <Link
-            key={ploeg.naam}
-            to={`/team/${ploeg.naam.toLowerCase()}`}
-            activeClassName={'font-bold'}
-            className={'text-gray-dark underline'}
-          >
-            {ploeg.naam}
-          </Link>
-        ))}
+        {/*todo: is this null check necessary or is it good that an error here would break the build?*/}
+        {category.ploeg &&
+          category.ploeg.map((ploeg) => (
+            <Link
+              key={ploeg.naam}
+              to={`/team/${ploeg.naam.toLowerCase()}`}
+              activeClassName={'font-bold'}
+              className={'text-gray-dark underline'}
+            >
+              {ploeg.naam}
+            </Link>
+          ))}
       </div>
     </div>
   )
