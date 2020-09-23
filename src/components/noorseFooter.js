72a8c850e3840300b0ee5460c3bbc9e9cf127961
gvdp <vdputteglenn@gmail.com>
2020-09-23 23:00:54 +0200

    feat: correct contact info

--- src/components/noorseFooter.js
+++ src/components/noorseFooter.js
@@ -106,9 +106,13 @@ export const ContactAndSponsorFooter = () => {
 export const ContactBlock = () => {
   return (
     <div className={`flex flex-col items-center`}>
-      <span>Frans de Peuterstraat</span>
-      <span>03/543.xx.xx</span>
-      <span>info@noorse.be</span>
+      <span>Frans de Peuterstraat 50</span>
+      <span>2950 Kapellen</span>
+      {/*todo: move to contentful*/}
+      <span>03/664.44.11</span>
+      <span>
+        <a href="mailto:info@noorse.be">info@noorse.be </a>
+      </span>
       <div className={'flex flex-row items-center mt-6 space-x-3'}>
         <ExternalLink
           url="https://www.facebook.com/noorsesv/"
