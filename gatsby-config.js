2460ade1be1d579a5138e72812076bf347d6fbeb
gvdp <vdputteglenn@gmail.com>
2020-09-26 14:19:38 +0200

    fix: only enable tracking upon env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,12 @@ const contentfulEnv =
   process.env.CONTENTFUL_ENV ||
   (process.env.PROD === 'true' ? 'master' : 'staging')
 const contentfulPreview = process.env.CONTENTFUL_PREVIEW === 'true'
+const tracking = process.env.TRACKING === 'true'
 
 console.log('Build settings:', {
   contentfulEnv,
   contentfulPreview,
+  tracking,
 })
 
 module.exports = {
@@ -20,21 +22,23 @@ module.exports = {
     author: `@gvdp`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GA_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Defers execution of google analytics script after page load
-        defer: true,
+    ...(tracking && [
+      ...{
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          // The property ID; the tracking code won't be generated without it
+          trackingId: process.env.GA_TRACKING_ID,
+          // Defines where to place the tracking script - `true` in the head and `false` in the body
+          head: true,
+          // Setting this parameter is optional
+          anonymize: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Defers execution of google analytics script after page load
+          defer: true,
+        },
       },
-    },
+    ]),
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
