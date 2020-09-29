c53f4895933e64c3420b1586e077ccf3d139237a
gvdp <vdputteglenn@gmail.com>
2020-09-29 22:06:47 +0200

    fix: properly spread card properties

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,30 +2,30 @@ import React from 'react'
 import { Clickable } from './a11y'
 import Img from 'gatsby-image'
 
-export const Card = (props) => {
+export const Card = ({ header, image, children, className, ...props }) => {
   return (
     <div
       {...props}
-      className={`${props.className} 
+      className={`${className} 
       bg-white elevation-2 
       m-auto w-full
       `}
       // max-w-400 sm:max-w-300
     >
-      {props.header && (
+      {header && (
         <div className={``}>
-          {props.image && (
+          {image && (
             <Img
-              fluid={props.image.fluid}
+              fluid={image.fluid}
               alt={'Card Header Image'}
               // className={'max-h-256p'}
               imgStyle={{ objectFit: 'scale-down' }}
             />
           )}
-          <h2 className={'text-center p-3 m-0 min-h-96p'}>{props.header}</h2>
+          <h2 className={'text-center p-3 m-0 min-h-96p'}>{header}</h2>
         </div>
       )}
-      {props.children && <div className={'w-full p-4'}>{props.children}</div>}
+      {children && <div className={'w-full p-4'}>{children}</div>}
     </div>
   )
 }
