48c8cd3939e0aa1ba6bbc11323d52e9d9a03f857
gvdp <vdputteglenn@gmail.com>
2020-10-06 11:02:42 +0200

    test:  add component test using cypress

--- backstop_data/engine_scripts/puppet/onBefore.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (page, scenario, vp) => {
-  // console.log(scenario)
-  if (process.env.CI) {
-    scenario.url = scenario.url.replace('host.docker.internal', 'localhost')
-  }
-  // console.log(scenario)
-}
---
+++ src/components/contact.cy-spec.js
@@ -0,0 +1,11 @@
+import '../../output.css'
+import React from 'react'
+import { mount } from 'cypress-react-unit-test'
+import { ContactInfo } from './contact'
+
+describe('ContactInfo', () => {
+  it('shows all contact info', () => {
+    mount(<ContactInfo />)
+    cy.contains('Frans de Peuterstraat 50')
+  })
+})
