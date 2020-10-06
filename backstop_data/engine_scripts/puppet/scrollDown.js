5da9a99f8faab3f562260cdd6dd515b1c6ba2251
gvdp <vdputteglenn@gmail.com>
2020-10-06 22:21:49 +0200

    test: remove backstop files

--- backstop_data/engine_scripts/puppet/scrollDown.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (page, scenario, vp) => {
-  await page.evaluate(() => {
-    window.scroll(0, 200)
-  })
-
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2000)
-  })
-}
