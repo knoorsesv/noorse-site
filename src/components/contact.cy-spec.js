372e9a24abc666acf1b35971eb872f3b167d4ce3
gvdp <vdputteglenn@gmail.com>
2020-10-08 21:57:14 +0200

    test: wrap component in container

--- src/components/contact.cy-spec.js
+++ src/components/contact.cy-spec.js
@@ -3,9 +3,17 @@ import React from 'react'
 import { mount } from 'cypress-react-unit-test'
 import { ContactInfo } from './contact'
 
+const ContactInContainer = () => {
+  return (
+    <div style={{ width: '200px', height: '200px' }}>
+      <ContactInfo />
+    </div>
+  )
+}
+
 describe('ContactInfo', () => {
   it('shows all contact info', () => {
-    mount(<ContactInfo />)
+    mount(<ContactInContainer />)
     cy.contains('Frans de Peuterstraat 50')
     cy.get('#contact-info').matchImageSnapshot('contact-info')
   })
