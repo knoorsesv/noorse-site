3025a9d37b4abc348806d701026a854af3ccbdb4
gvdp <vdputteglenn@gmail.com>
2020-10-09 15:57:37 +0200

    test: add check to see if each category page is generated

--- cypress/integration/category-page.cy-spec.js
+++ cypress/integration/category-page.cy-spec.js
@@ -1,27 +1,38 @@
 describe('Category page', () => {
-  beforeEach(() => {
-    cy.visit('/senioren')
+  const categories = ['Senioren', 'Jeugd', 'Dames', 'Meisjes', 'G-Voetbal']
+
+  categories.forEach((category) => {
+    it(`should be generated for ${category}`, () => {
+      cy.visit(`/${category.toLowerCase()}`)
+      cy.get('#content-wrapper').should('contain', category)
+    })
   })
 
-  it('shows list of ploegen', () => {
-    cy.contains('Ploegen')
-      .parent()
-      .within(() => {
-        cy.get('a')
-          .should('contain', 'Noorse 1')
-          .should('contain', 'Noorse 3')
-          .should('contain', 'Reserven B')
-          .should('contain', 'Reserven A')
-      })
-  })
+  describe('content', () => {
+    beforeEach(() => {
+      cy.visit('/senioren')
+    })
 
-  it('shows list of news items', () => {
-    cy.contains('Nieuws')
-      .parent()
-      .within(() => {
-        cy.get('a')
-          .should('contain', 'Nieuwe spelers 2020-2021')
-          .should('contain', 'Noorse gaat door met trainersduo')
-      })
+    it('shows list of ploegen', () => {
+      cy.contains('Ploegen')
+        .parent()
+        .within(() => {
+          cy.get('a')
+            .should('contain', 'Noorse 1')
+            .should('contain', 'Noorse 3')
+            .should('contain', 'Reserven B')
+            .should('contain', 'Reserven A')
+        })
+    })
+
+    it('shows list of news items', () => {
+      cy.contains('Nieuws')
+        .parent()
+        .within(() => {
+          cy.get('a')
+            .should('contain', 'Nieuwe spelers 2020-2021')
+            .should('contain', 'Noorse gaat door met trainersduo')
+        })
+    })
   })
 })
