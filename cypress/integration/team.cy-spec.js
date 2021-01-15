3c1a09b9654324291d71acc295a6ff14ebfd9155
gvdp <vdputteglenn@gmail.com>
2021-01-15 15:58:49 +0100

    test: placeholder test

---
+++ cypress/integration/team.cy-spec.js
@@ -0,0 +1,5 @@
+it('should have links to other teams in same category', function () {})
+
+it('should show results fetched from kbvb graphql site', () => {})
+
+it('should show team info fetched from contentful', function () {})
