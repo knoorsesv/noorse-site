ef9163c9e0870dca2988a050ba2ab0466605c4f1
gvdp <vdputteglenn@gmail.com>
2021-01-17 21:32:38 +0100

    test: navigation tests

--- cypress/integration/pages/navigation.cy-spec.js
+++ cypress/integration/pages/navigation.cy-spec.js
@@ -16,10 +16,23 @@ describe('Navigation', function () {
       .and('equal', 'https://www.qlub.com/qlub?club=k00332')
   })
 
-  it('should be possible to go to bestuur page')
-  it('should be possible to go to contact page')
-  it('should be possible to go to all category pages')
-  it('should be possible to go to lidmaatschap page')
-  it('should be possible to go to documenten page')
-  it('should be possible to go to sponsoring page')
+  it('should be possible to go to info pages', () => {
+    cy.contains('Info').get('#dropdown').invoke('show')
+    //todo: could this be done automatically to check if all generated pages have a link?
+    cy.contains('Bestuur').should('have.attr', 'href', '/bestuur')
+    cy.contains('Fair Play').should('have.attr', 'href', '/fairplay')
+    cy.contains('Lidmaatschap').should('have.attr', 'href', '/lidmaatschap')
+    cy.contains('Sponsoring').should('have.attr', 'href', '/sponsoring')
+    cy.contains('Documenten').should('have.attr', 'href', '/documenten')
+  })
+
+  it('should be possible to go to all category pages', () => {
+    cy.get('nav').contains('Senioren').should('have.attr', 'href', '/senioren')
+    cy.get('nav').contains('Jeugd').should('have.attr', 'href', '/jeugd')
+    cy.get('nav')
+      .contains('G-Voetbal')
+      .should('have.attr', 'href', '/g-voetbal')
+    cy.get('nav').contains('Meisjes').should('have.attr', 'href', '/meisjes')
+    cy.get('nav').contains('Dames').should('have.attr', 'href', '/dames')
+  })
 })
