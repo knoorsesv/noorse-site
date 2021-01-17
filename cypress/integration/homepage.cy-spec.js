7a04f49996e9f38d1163fba06c6e33597932eca9
gvdp <vdputteglenn@gmail.com>
2021-01-17 15:39:21 +0100

    test: some more cy tests

--- cypress/integration/homepage.cy-spec.js
+++ cypress/integration/homepage.cy-spec.js
@@ -14,8 +14,60 @@ describe('Home Page', function () {
       })
   })
 
-  it('should have a trooper section which links to Noorse trooper page')
+  it('should have a trooper section which links to Noorse trooper page', () => {
+    cy.contains('h1', 'Trooper')
+      .parents('section')
+      .within(() => {
+        cy.get('a')
+          .should('have.attr', 'target', '_blank')
+          .and('have.prop', 'href')
+          .and('equal', 'https://www.trooper.be/noorse')
+      })
+  })
 
-  it('should go to news item when clicking card')
-  it('should go to news item when selecting card and pressing key down')
+  it('should list news items', () => {
+    cy.contains('h1', 'Nieuws')
+      .parents('section')
+      .within(() => {
+        cy.get('article').should('have.length', 6)
+        cy.contains('Nieuwe spelers 2020-2021')
+        cy.contains('Anita Brand nieuwe T1 Noorse Dames')
+        cy.contains('Samenwerking Corpus Fit')
+        //  ...
+      })
+  })
+
+  it('should go to news item when clicking card', () => {
+    cy.contains('Nieuwe spelers 2020-2021')
+      .parents('article')
+      .should('have.attr', 'role', 'link')
+      .click()
+      .then(() => {
+        cy.url().should('contain', '/nieuws/Nieuwe%20spelers%202020-2021')
+      })
+  })
+
+  it('should go to news item when selecting card and pressing key down', () => {
+    // todo: should this be tested here or is the react a11y linting enough?
+  })
+
+  it('should list events', () => {
+    cy.contains('h1', 'Evenementen')
+      .parents('section')
+      .within(() => {
+        cy.contains('01/05/21')
+        cy.contains('6 Tegen 6')
+      })
+  })
+
+  it.only('should list sponsors in footer', () => {
+    cy.get('footer [title="List of Sponsors"]')
+      .last()
+      .scrollIntoView()
+      // .should('be.visible')
+      .within(() => {
+        cy.get('.lazyload-placeholder').should('have.length', 5)
+        // .scrollIntoView()scrollIntoViewß
+      })
+  })
 })
