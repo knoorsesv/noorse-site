7511a377ce0410157d68780841e7d4de7ca21bfe
gvdp <vdputteglenn@gmail.com>
2021-01-17 22:34:55 +0100

    test: sort games

--- src/templates/team-page-template.js
+++ src/templates/team-page-template.js
@@ -56,39 +56,42 @@ function CalendarTable({ calendar }) {
   return (
     <table className={'w-full table-fixed border-separate text-sm lg:w-4/5'}>
       <tbody>
-        {calendar.filter(notCancelled).map((game) => {
-          const parsedDate = parseISO(game.startDate)
-          game.formattedDate = format(parsedDate, 'dd/MM')
-          game.time = format(parsedDate, 'HH:mm')
-          return (
-            <tr key={game.id} className={'pb-1'}>
-              <td className={'text-sm w-2/12'}>
-                <div>{game.formattedDate}</div>
-                <div>{game.time}</div>
-              </td>
-              <td className={'w-7/12 px-0'}>
-                <div className={'flex flex-col'}>
-                  <span className={'truncate'}>
-                    {formatTeamName(game.homeTeam)}
-                  </span>
-                  <span className={'truncate'}>
-                    {formatTeamName(game.awayTeam)}
-                  </span>
-                </div>
-              </td>
-              <td className={'w-1/12 px-1 text-center'}>
-                {game.outcome.status === 'finished' ? (
-                  <div className={'flex flex-col items-center'}>
-                    <span>{game.outcome.homeTeamGoals}</span>
-                    <span>{game.outcome.awayTeamGoals}</span>
+        {calendar
+          .filter(notCancelled)
+          .sort()
+          .map((game) => {
+            const parsedDate = parseISO(game.startDate)
+            game.formattedDate = format(parsedDate, 'dd/MM')
+            game.time = format(parsedDate, 'HH:mm')
+            return (
+              <tr key={game.id} className={'pb-1'}>
+                <td className={'text-sm w-2/12'}>
+                  <div>{game.formattedDate}</div>
+                  <div>{game.time}</div>
+                </td>
+                <td className={'w-7/12 px-0'}>
+                  <div className={'flex flex-col'}>
+                    <span className={'truncate'}>
+                      {formatTeamName(game.homeTeam)}
+                    </span>
+                    <span className={'truncate'}>
+                      {formatTeamName(game.awayTeam)}
+                    </span>
                   </div>
-                ) : (
-                  <span>-</span>
-                )}
-              </td>
-            </tr>
-          )
-        })}
+                </td>
+                <td className={'w-1/12 px-1 text-center'}>
+                  {game.outcome.status === 'finished' ? (
+                    <div className={'flex flex-col items-center'}>
+                      <span>{game.outcome.homeTeamGoals}</span>
+                      <span>{game.outcome.awayTeamGoals}</span>
+                    </div>
+                  ) : (
+                    <span>-</span>
+                  )}
+                </td>
+              </tr>
+            )
+          })}
       </tbody>
     </table>
   )
