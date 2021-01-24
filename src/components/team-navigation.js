be0c7246c1afc3d1b1b46c8201c06b1d26878a4d
gvdp <vdputteglenn@gmail.com>
2021-01-24 13:38:30 +0100

    fix: add trailing slash to try and fix active link styling

--- src/components/team-navigation.js
+++ src/components/team-navigation.js
@@ -16,7 +16,8 @@ export const CategoryTeamNavigation = ({ category, header }) => {
           category.ploeg.map((ploeg) => (
             <Link
               key={ploeg.naam}
-              to={`/team/${ploeg.naam.toLowerCase()}`}
+              to={`/team/${ploeg.naam.toLowerCase()}/`}
+              partiallyActive={true}
               activeClassName={'font-bold'}
               className={'text-gray-dark underline mx-3'}
             >
