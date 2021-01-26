1ca9bac6e39300c40f0c3cd71b1de2f14d6bf863
gvdp <vdputteglenn@gmail.com>
2021-01-26 21:07:04 +0100

    refactor: only use 1 nav list and style it conditionally

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ import { ExternalLink } from './text'
 import { siteMap as defaultSiteMap } from '../env/constants'
 import { CoverImage } from './cover-image'
 
-const transition = `transition-all duration-1000 ease-in`
+const transition = `transition-all duration-200 ease-in`
 const menuBarHeight = 'h-64p sm:h-80p'
 const coverSectionHeight = 'h-32v sm:h-48v lg:64v'
 
@@ -53,17 +53,22 @@ const NavSection = ({
 }
 
 const MenuItemList = ({ topMenuBarShown, siteMap, sideBarMenuShown }) => {
-  return [
+  return (
     <ul
       id="menu-list"
       className={`
-      fixed top-0 pt-16 pr-4
-      w-1/2 sm:w-2/5
-      list-none h-full flex flex-col space-y-1 sm:space-y-2 ${transition}
+      fixed top-0 
+      flex flex-col lg:flex-row lg:justify-end lg:items-center
+      space-y-1 sm:space-y-2 lg:space-y-0
+      pt-16 pr-4 lg:pr-6
+      w-1/2 sm:w-2/5 
+      list-none z-50
+      ${transition}
+      ${!sideBarMenuShown ? 'opacity-0 -right-1/2 lg:right-0' : ''}
       ${
-        !sideBarMenuShown
-          ? 'opacity-0 -right-1/2'
-          : 'bg-green opacity-100 right-0'
+        sideBarMenuShown || topMenuBarShown
+          ? 'h-full lg:h-80p bg-green opacity-100 right-0 lg:w-full lg:py-10'
+          : 'lg:right-0 lg:w-auto lg:h-16 lg:top-6 lg:pt-0 lg:pl-5 lg:opacity-90 lg:bg-green'
       }
       `}
     >
@@ -72,7 +77,7 @@ const MenuItemList = ({ topMenuBarShown, siteMap, sideBarMenuShown }) => {
           id={'menu-item'}
           key={item.name}
           className={`text-right lg:text-center
-      lg:my-2 lg:mx-3
+        lg:mx-2 xl:px-3 lg:py-3
       ${transition}
       ${item.subItems && 'group'}`}
         >
@@ -82,8 +87,8 @@ const MenuItemList = ({ topMenuBarShown, siteMap, sideBarMenuShown }) => {
           )}
         </li>
       ))}
-    </ul>,
-  ]
+    </ul>
+  )
 }
 
 const SubMenuItems = ({ item }) => {
@@ -92,8 +97,8 @@ const SubMenuItems = ({ item }) => {
       id="dropdown"
       className={`
       lg:hidden lg:group-hover:absolute lg:group-hover:flex
-      flex flex-col space-y-2 lg:items-start
-      w-auto lg:p-6 mr-4
+      flex flex-col space-y-2 lg:items-start lg:-ml-1
+      w-auto lg:p-6 mr-4 lg:bg-green lg:opacity-90
       ${transition}`}
     >
       {item.subItems.map((subItem) => {
@@ -102,38 +107,6 @@ const SubMenuItems = ({ item }) => {
     </ul>
   )
 }
-
-// const SideBarMenu = ({ topMenuBarShown, siteMap, initiallyShown }) => {
-//
-//
-//   return (
-//     <div
-//       id="sidebar-container"
-//       className={`${transition}
-//       lg:hidden block
-//       z-50
-//      h-screen fixed right-0 top-0 p-4 sm:pr-8
-//      ${menuShown ? 'w-1/2 sm:w-1/3' : 'w-0'}
-//      ${menuShown && 'bg-green'}
-//    `}
-//     >
-//
-//
-//       <MenuItemList topMenuBarShown={topMenuBarShown} siteMap={siteMap}/>
-//       {/*<ul*/}
-//       {/*  id="sidebar-menu"*/}
-//       {/*  className={`*/}
-//       {/*      list-none*/}
-//       {/*      ${menuShown ? 'block' : 'hidden'}*/}
-//       {/*      flex flex-col justify-between mt-6 sm:mt-8`}*/}
-//       {/*>*/}
-//       {/*  {siteMap.items.map((item) => (*/}
-//       {/*    <MenuItem key={item.name} item={item} topMenuBarShown={fixedToTop} />*/}
-//       {/*  ))}*/}
-//       {/*</ul>*/}
-//     </div>
-//   )
-// }
 
 const NavLink = ({ item }) => {
   const className = 'text-white text-lg'
@@ -188,14 +161,12 @@ export const Navbar = ({
         pageHasCoverPhoto={pageHasCoverPhoto}
         setTopMenuBarShown={setTopMenuBarShown}
       >
-        <MenuLogo topMenuBarShown={topMenuBarShown} />
-
         <nav
           className={`
           ${transition}
           fixed w-full
           pr-4
-          ${topMenuBarShown ? `z-20 bg-green ${menuBarHeight}` : 'h-0 p-0'}
+          ${topMenuBarShown ? `z-20 bg-green ${menuBarHeight}` : 'z-50 h-0 p-0'}
           `}
         >
           <MenuItemList
@@ -205,6 +176,7 @@ export const Navbar = ({
           />
           {/*<MenuBackground topMenuBarShown={topMenuBarShown}/>*/}
         </nav>
+        <MenuLogo topMenuBarShown={topMenuBarShown} />
         <MenuToggle
           clickBurger={toggleMenuShown}
           sideBarMenuShown={sideBarMenuShown}
@@ -212,25 +184,6 @@ export const Navbar = ({
         />
       </NavSection>
     </React.Fragment>
-  )
-}
-
-const MenuBackground = ({ topMenuBarShown }) => {
-  return (
-    <div
-      key="menu-background"
-      className={`${topMenuBarShown && 'hidden'} ${transition} shadow-lg`}
-      style={{
-        position: 'relative',
-        top: '-35px',
-        borderStyle: 'solid',
-        borderWidth: '0 0 40px 20px',
-        width: '100%',
-        zIndex: '0',
-        opacity: '70%',
-        borderColor: 'transparent transparent green transparent',
-      }}
-    />
   )
 }
 
@@ -257,7 +210,7 @@ const MenuToggle = ({ clickBurger, sideBarMenuShown, topMenuBarShown }) => {
     <div
       className={`fixed right-0 
         lg:hidden
-        mt-4 mr-3 sm:mt-4 
+        mt-4 mr-3 sm:mt-6 sm:mr-4 
         p-2 z-50 text-white
         ${sideBarMenuShown || topMenuBarShown ? '' : 'bg-green'} ${transition}`}
     >
