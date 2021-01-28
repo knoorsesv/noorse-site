4aa70cfaf78cf0bd3b7ac603324212546989d219
gvdp <vdputteglenn@gmail.com>
2021-01-28 20:04:19 +0100

    Revert "feat: add tags to better share on fb"
    This reverts commit b0792396048d362a4fa63ba62044bdf5ccbd5aad.

--- src/components/news-card.js
+++ src/components/news-card.js
@@ -3,7 +3,23 @@ import React from 'react'
 import { ClickableCard, SubHeader } from './cards'
 
 export const NewsCard = ({ newsNode }) => {
-  const snippet = createSnippetFromNewsPost(newsNode)
+  const maxChars = 100
+  let snippet = ''
+  let paragraph = -1
+  const firstContent = newsNode.body.json.content[0].content
+  while (snippet.length < maxChars && paragraph++ < firstContent.length - 1) {
+    const paragraphContent = firstContent[paragraph]
+    let extraText
+    if (paragraphContent.nodeType === 'text') {
+      extraText = paragraphContent.value
+    }
+
+    if (paragraphContent.nodeType === 'hyperlink') {
+      extraText = paragraphContent.content[0].value
+    }
+    snippet = (snippet + ' ' + extraText).substr(0, maxChars)
+  }
+  snippet += ' ...'
 
   const goToNews = () => {
     navigate(`/nieuws/${newsNode.title}`)
@@ -25,24 +41,4 @@ export const NewsCard = ({ newsNode }) => {
       <div className={'text-center min-h-64p'}>{snippet}</div>
     </ClickableCard>
   )
-}
-export function createSnippetFromNewsPost(newsNode) {
-  const maxChars = 100
-  let snippet = ''
-  let paragraph = -1
-  const firstContent = newsNode.body.json.content[0].content
-  while (snippet.length < maxChars && paragraph++ < firstContent.length - 1) {
-    const paragraphContent = firstContent[paragraph]
-    let extraText
-    if (paragraphContent.nodeType === 'text') {
-      extraText = paragraphContent.value
-    }
-
-    if (paragraphContent.nodeType === 'hyperlink') {
-      extraText = paragraphContent.content[0].value
-    }
-    snippet = (snippet + ' ' + extraText).substr(0, maxChars)
-  }
-  snippet += ' ...'
-  return snippet
 }
