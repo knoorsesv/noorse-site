af176c4f0215aa64eff8446bf2acd3899a071d51
gvdp <vdputteglenn@gmail.com>
2021-06-12 13:45:05 +0200

    test: update cypress and fix tests

--- cypress/src/tests/templates/category-page.cy-spec.js
+++ cypress/src/tests/templates/category-page.cy-spec.js
@@ -14,15 +14,17 @@ describe('Category page', () => {
     })
 
     it('shows list of ploegen', () => {
-      cy.contains('Ploegen')
-        .parent()
-        .within(() => {
-          cy.get('a')
-            .should('contain', 'Noorse 1')
-            .should('contain', 'Noorse 3')
-            .should('contain', 'Reserven B')
-            .should('contain', 'Reserven A')
-        })
+      cy.get('#content-wrapper').within(() => {
+        cy.contains('Ploegen')
+          .parent()
+          .within(() => {
+            cy.get('a')
+              .should('contain', 'Noorse 1')
+              .should('contain', 'Noorse 3')
+              .should('contain', 'Reserven B')
+              .should('contain', 'Reserven A')
+          })
+      })
     })
 
     it('shows list of news items', () => {
