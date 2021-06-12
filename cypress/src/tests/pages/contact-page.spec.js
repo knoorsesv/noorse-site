768ea009e7b4207ebaf0634a9aab2795ba252943
gvdp <vdputteglenn@gmail.com>
2021-06-12 16:00:57 +0200

    test: intentionally fail

--- cypress/src/tests/pages/contact-page.spec.js
+++ cypress/src/tests/pages/contact-page.spec.js
@@ -1,6 +1,6 @@
 it('should have contact page', () => {
   cy.visit('/contact')
-  cy.contains('h1', 'Koninklijke Noorse SV')
+  cy.contains('h1', 'Konddddddddinklijke Noorse SV')
   cy.contains('h1', 'Contact')
   cy.contains('h1', 'Bereikbaarheid')
   cy.contains('h1', 'Kantine')
