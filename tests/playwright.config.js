e67d3901b7aea7672e7dd4e22fa604b0e37ce18d
gvdp <vdputteglenn@gmail.com>
2022-01-08 14:50:10 +0100

    fix test run

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -3,14 +3,15 @@
 const { devices } = require('@playwright/test')
 require('dotenv').config()
 
-console.log('running this on CI? ', process.env.CI)
+const runOnCI = process.env.CI === 'true'
+console.log('running this on CI? ', runOnCI)
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-  reporter: process.env.CI
+  reporter: runOnCI
     ? 'github'
-    : [['list'], ['html', { open: process.env.CI ? 'never ' : 'on-failure' }]],
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+    : [['list'], ['html', { open: runOnCI ? 'never ' : 'on-failure' }]],
+  forbidOnly: !!runOnCI,
+  retries: runOnCI ? 2 : 0,
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:8000',
     trace: 'on-first-retry',
