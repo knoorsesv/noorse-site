48c42f1ede63e8e1898df7a645ccce9a3eebdb67
gvdp <vdputteglenn@gmail.com>
2022-02-22 20:38:20 +0100

    ci: conditionally replace base url for Docker

--- tests/run-test-update.sh
+++ tests/run-test-update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "running playwright test"
+
+export RUN_IN_DOCKER=true
+
+
 # todo: do this in Dockerfile, but for now can't because node_modules gets overwritten by the volume in docker run
 npm install
 
