d58ad4924b03b658de742c47062aa00bf41d6f6d
Glenn Van De Putte <glenn.vandeputte@bagaar.be>
2022-06-23 00:07:35 +0200

    feat: also include bold for new open source font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import '@fontsource/lato'
 import '@fontsource/lato/700.css'
 import '@fontsource/mulish'
+import '@fontsource/mulish/700.css'
 
 import './src/styles/tailwind-import.css'
 import './src/styles/global.scss'
