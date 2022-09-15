86ceb09137a9ef3d4161ed6d853e74652ebd03cf
gvdp <vdputteglenn@gmail.com>
2022-09-15 21:51:14 +0200

    fix: correct svg properties to react specific

--- src/components/text.js
+++ src/components/text.js
@@ -40,15 +40,15 @@ export const ExternalLink = ({
       {icon && (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 max-w-sm pl-1"
+          className="h-5 w-5 max-w-sm pl-1"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth="2"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
           />
         </svg>
@@ -66,15 +66,15 @@ export const EmailLink = ({ address }) => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="mr-1 h-4 w-4"
+        className="mr-1 h-4 w-4"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
+        strokeWidth="2"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
         />
       </svg>
