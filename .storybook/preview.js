9431623dc44cf70e981bf9d12b452e0ed3f5591c
gvdp <vdputteglenn@gmail.com>
2022-10-15 18:11:02 +0200

    fix: import styles in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import '../src/styles/global.scss'
+import '../src/styles/tailwind-import.css'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: { default: 'light' },
