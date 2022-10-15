ac058c88b9c891b41935e7345adbabf630b43f50
gvdp <vdputteglenn@gmail.com>
2022-10-15 17:01:13 +0200

    feat: add storybook setup

---
+++ src/components/text.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { TextBlock } from './text'
+
+export const RandomText = () => <TextBlock>Some random styled text</TextBlock>
+
+export default {
+  title: 'Text/Block',
+  component: TextBlock,
+}
