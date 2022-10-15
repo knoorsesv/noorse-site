5784b8ca3aabfb2ff1ece448f2ab677bf18f0e48
gvdp <vdputteglenn@gmail.com>
2022-10-15 18:11:23 +0200

    chore: add stories for external link

---
+++ src/components/link.stories.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ExternalLink } from './text'
+
+const Link = (args) => (
+  <ExternalLink {...args} url="http://www.noorse.be">
+    Go Outside
+  </ExternalLink>
+)
+
+export default {
+  title: 'Text/Link',
+  component: ExternalLink,
+  argTypes: {
+    icon: false,
+    styled: true,
+    altText: 'Link naar site',
+    textColor: '',
+  },
+}
+
+export const LinkWithIcon = Link.bind({})
+LinkWithIcon.args = {
+  icon: true,
+}
+
+export const LinkWithoutIcon = Link.bind({})
+LinkWithoutIcon.args = {
+  icon: false,
+}
+
+export const UnstyledLink = Link.bind({})
+UnstyledLink.args = {
+  styled: false,
+}
+
+export const LinkWithTextColor = Link.bind({})
+LinkWithTextColor.args = {
+  textColor: 'text-green',
+}
