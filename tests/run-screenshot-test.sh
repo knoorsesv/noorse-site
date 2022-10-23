f24388a789aadf8dd2b4118c831b1884c52710af
gvdp <vdputteglenn@gmail.com>
2022-10-23 23:32:05 +0200

    ci: run functional and screenshot tests separately

--- tests/run-screenshot-test.sh
+++ tests/run-screenshot-test.sh
@@ -3,4 +3,4 @@ echo "running playwright test"
 export RUN_IN_DOCKER=true
 # todo: do this in Dockerfile, but for now can't because node_modules gets overwritten by the volume in docker run
 yarn
-yarn test:func
+yarn test screenshots
