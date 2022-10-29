733bbac5f0ddc45780c9034b05328258192b9cea
gvdp <vdputteglenn@gmail.com>
2022-10-29 15:47:13 +0200

    chore: inject typography in story book to apply same styling as on website

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import '../src/styles/global.scss'
 import '../src/styles/tailwind-import.css'
+import typography from '../src/utils/typography'
+// todo: maybe the storybook shouldn't rely on this? there are "reset" classes on ul tags for example without which the components dont look good isolated
+typography.injectStyles()
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
