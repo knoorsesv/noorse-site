72afc0af79b3a9a3d9ea3db3e28c618c526dfb9f
gvdp <vdputteglenn@gmail.com>
2022-11-05 22:39:39 +0100

    fix: dont put duplicate id on image which is shown twice on the page

--- tests/screenshots.spec.js
+++ tests/screenshots.spec.js
@@ -21,7 +21,7 @@ test.describe.parallel('Screenshot Test', () => {
       await page.goto(pageUrl)
       await page.waitForLoadState('networkidle')
 
-      await (await page.$('#noorse-logo')).waitForElementState('stable')
+      await (await page.$('[alt="Noorse Logo"]')).waitForElementState('stable')
       // todo: maybe scroll on big pages? / open navbar on mobile?
       expect(
         await page.screenshot({ fullPage: takeFullPage.includes(pageUrl) })
