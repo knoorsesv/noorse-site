32897d5b961beee65bd3369c4454baa79662ca16
gvdp <vdputteglenn@gmail.com>
2022-11-20 00:02:02 +0100

    feat: collapse submenu items in mobile navbar

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -127,3 +127,30 @@ export const Calendar = () => (
     />
   </svg>
 )
+export const ChevronRight = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12px"
+    height="12px"
+    viewBox="0 0 384 512"
+    fill="currentColor"
+    stroke="currentColor"
+    {...props}
+  >
+    <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+  </svg>
+)
+
+export const ChevronDown = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    width="12px"
+    height="12px"
+    fill="currentColor"
+    stroke="currentColor"
+    {...props}
+  >
+    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+  </svg>
+)
