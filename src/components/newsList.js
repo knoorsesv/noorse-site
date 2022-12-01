1732562f212bdc560aa511805d9837255ea9edd0
gvdp <vdputteglenn@gmail.com>
2022-12-01 23:00:00 +0100

    FIx: just use an a tag instead of onclick for news cards

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import { NewsCard } from './news-card'
 
-export const NewsList = ({
-  children,
-  NewsCardImage,
-  shownNewsItems,
-  onClick,
-}) => {
+export const NewsList = ({ children, NewsCardImage, shownNewsItems }) => {
   return (
     <section className={'flex flex-col flex-wrap medium:flex-row'}>
       {shownNewsItems.map((node) => (
         <div key={node.title} className={'mb-[10px] medium:w-1/2 medium:px-2'}>
-          <NewsCard
-            newsNode={node}
-            NewsCardImage={NewsCardImage}
-            onClick={onClick}
-          />
+          <NewsCard newsNode={node} NewsCardImage={NewsCardImage} />
         </div>
       ))}
       <div className={'my-4 flex w-full justify-center text-xl'}>
