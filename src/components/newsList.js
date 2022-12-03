719a28f1de5842f0e76faeacbe68685243a1171b
gvdp <vdputteglenn@gmail.com>
2022-12-03 16:00:25 +0100

    feat: use sectionlist instead of grid on home page

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -3,9 +3,14 @@ import { NewsCard } from './news-card'
 
 export const NewsList = ({ children, NewsCardImage, shownNewsItems }) => {
   return (
-    <section className={'flex flex-col flex-wrap medium:flex-row'}>
+    <section
+      className={'flex flex-col flex-wrap justify-between medium:flex-row'}
+    >
       {shownNewsItems.map((node) => (
-        <div key={node.title} className={'mb-[10px] medium:w-1/2 medium:px-2'}>
+        <div
+          key={node.title}
+          className={'mb-[10px] medium:basis-1/2 medium:px-2 large:basis-1/3 '}
+        >
           <NewsCard newsNode={node} NewsCardImage={NewsCardImage} />
         </div>
       ))}
