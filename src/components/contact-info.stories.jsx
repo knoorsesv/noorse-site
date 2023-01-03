df0787f0e0020e247f4332049ed0248d778c6217
gvdp <vdputteglenn@gmail.com>
2023-01-03 22:07:11 +0100

    refactor: rewrite contact-info component to jsx

---
+++ src/components/contact-info.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ContactInfo } from './contact-info.jsx'
+
+const Template = () => <ContactInfo />
+
+export default {
+  title: 'Component/ContactInfo',
+  component: ContactInfo,
+}
+
+export const Default = Template.bind({})
