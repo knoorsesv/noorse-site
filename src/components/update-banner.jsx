a5a60fe2f7be15ea99018112b642df6ba23b5e97
Glenn Van De Putte <glenn.vandeputte@bagaar.be>
2023-06-02 09:55:01 +0200

    feat: add banner to indicate info needs updating

---
+++ src/components/update-banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Exclamation } from './icons/icons.jsx'
+
+export const UpdateBanner = () => {
+  return (
+    <div className="flex w-full items-center rounded-lg bg-gray-300 p-8">
+      <Exclamation className="color-black mr-4 h-10 w-10" />
+      <span className="text-lg">
+        Niet alle info op de website is volledig up to date. We werken eraan om
+        dit zo snel mogelijk volledig in orde te krijgen.
+      </span>
+    </div>
+  )
+}
