f00a0ea3669d10434b9fa3cdce7e257fc9bd4443
gvdp <vdputteglenn@gmail.com>
2023-06-03 09:21:32 +0200

    feat: update banner

--- src/components/update-banner.jsx
+++ src/components/update-banner.jsx
@@ -4,7 +4,7 @@ import { Exclamation } from './icons/icons.jsx'
 export const UpdateBanner = () => {
   return (
     <div className="flex w-full items-center rounded-lg bg-gray-300 p-8">
-      <Exclamation className="color-black mr-4 h-10 w-10" />
+      <Exclamation className="color-black mr-5 h-10 w-10 shrink-0" />
       <span className="text-lg">
         Niet alle info op de website is volledig up to date. We werken eraan om
         dit zo snel mogelijk volledig in orde te krijgen.
