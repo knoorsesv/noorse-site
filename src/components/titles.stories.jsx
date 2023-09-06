6092eaecf0aee58160bbc47f9f20f9a3acc931a3
gvdp <vdputteglenn@gmail.com>
2023-09-06 22:55:15 +0200

    chore: add story for title components

---
+++ src/components/titles.stories.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SubTitle, Title } from './titles.jsx'
+
+const Template = () => (
+  <>
+    <Title>Title</Title>
+    <SubTitle>Subtitle</SubTitle>
+  </>
+)
+
+export default {
+  title: 'Component/Title',
+  component: Title,
+}
+
+export const Default = Template.bind({})
