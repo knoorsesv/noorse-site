fff2a36568320727a4fe5b02593d64992868ecca
gvdp <vdputteglenn@gmail.com>
2023-09-20 22:54:41 +0200

    refactor: extract infoBestuurPage to pure React component

--- src/pages/info/bestuur.js
+++ src/pages/info/bestuur.js
@@ -1,9 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { BestuurList, SubTitle, Title } from '../../components'
-import { Container } from '../../components/layout'
-import Layout from '../../layouts/layout'
+import { InfoBestuurPage } from '../../components/pages'
 
 const query = graphql`
   query {
@@ -22,41 +19,7 @@ const query = graphql`
 
 const Bestuur = () => {
   const data = useStaticQuery(query)
-
-  const bestuursLeden = data.allContentfulBestuurslid.edges
-    .map(({ node }) => node)
-    .filter(({ type }) => type === 'bestuursorgaan')
-    .sort((a, b) => {
-      if (a.title === 'Voorzitter') return -1
-      if (b.title === 'Voorzitter') return 1
-      return 0
-    })
-  const deelwerkingen = data.allContentfulBestuurslid.edges
-    .map(({ node }) => node)
-    .filter(({ type }) => type === 'deelwerking')
-
-  return (
-    <Layout>
-      <Helmet>
-        <title>Bestuur</title>
-      </Helmet>
-      <Container centered={false}>
-        <Title>Bestuur</Title>
-        <SubTitle>Organigram</SubTitle>
-        <img
-          className="max-w-screen-md medium:w-[800px] medium:max-w-full  m-8"
-          alt="Bestuursorganigram"
-          src={
-            'https://www.mermaidchart.com/raw/8ba49245-d4d8-455f-ad19-50cccfa42034?version=v0.1&theme=light&format=svg'
-          }
-        />
-        <SubTitle>Bestuursorgaan</SubTitle>
-        <BestuurList leden={bestuursLeden} />
-        <SubTitle>Deelwerkingen</SubTitle>
-        <BestuurList leden={deelwerkingen} />
-      </Container>
-    </Layout>
-  )
+  return <InfoBestuurPage leden={data.allContentfulBestuurslid.edges} />
 }
 
 export default Bestuur
