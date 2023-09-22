942efabb259febc6d349138cb1e40b5c34717f7c
gvdp <vdputteglenn@gmail.com>
2023-09-22 21:07:25 +0200

    refactor: extract lidmaatschap to pure react page

--- src/pages/info/lidmaatschap.js
+++ src/pages/info/lidmaatschap.js
@@ -1,8 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { Attachments, MarkDown, Title } from '../../components'
-import { Container } from '../../components/layout'
+import { InfoLidmaatschapPage } from '../../components/pages'
 import Layout from '../../layouts/layout'
 
 const LidMaatschapPage = () => {
@@ -29,31 +27,9 @@ const LidMaatschapPage = () => {
 
   const content = data.allContentfulPage.nodes[0]
 
-  // todo: make this a <MarkDownPage />
-
   return (
     <Layout>
-      <Helmet>
-        <title>Lid Worden</title>
-        <meta property="og:title" content={`Lid Worden`} />
-        <meta
-          property="og:description"
-          content={`Wil je lid worden van één van onze jeugd- of meisjesploegen? Hier vindt je alle info.`}
-        />
-      </Helmet>
-      <Container>
-        <Title>Lid Worden</Title>
-        {content ? (
-          <>
-            <MarkDown>{content.body.body}</MarkDown>
-            <section className={'prose'}>
-              <Attachments attachments={content.attachment} />
-            </section>
-          </>
-        ) : (
-          <section>No content</section>
-        )}
-      </Container>
+      <InfoLidmaatschapPage content={content} />
     </Layout>
   )
 }
