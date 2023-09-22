d7dd01845c29b655f048d7dad6ff7d367a7f39aa
gvdp <vdputteglenn@gmail.com>
2023-09-22 21:13:54 +0200

    refactor: extract news page to pure react component

--- src/pages/info/nieuws.js
+++ src/pages/info/nieuws.js
@@ -1,43 +1,18 @@
-import { navigate } from 'gatsby-link'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { NewsList, Title } from '../../components'
-import { Container } from '../../components/layout'
+import { NewsPage } from '../../components/pages'
 import Layout from '../../layouts/layout'
 import { getConstrainedLogoData } from '../../queries/constrained-logo'
 import { getNewsItems } from '../../queries/news'
 
-const NewsCardImage = ({ image }) => {
-  const fallBackLogo = getConstrainedLogoData()
-
-  return (
-    <div className={'h-[200px] text-center'}>
-      <GatsbyImage
-        image={image?.gatsbyImageData || fallBackLogo.gatsbyImageData}
-        alt={'Card Header'}
-      />
-    </div>
-  )
-}
-
 const Nieuws = () => {
-  const goToNews = (newsNode) => {
-    navigate(`/nieuws/${newsNode.title}`)
-  }
   return (
     <Layout>
-      <Helmet>
-        <title>Nieuws</title>
-      </Helmet>
-      <Container>
-        <Title>Nieuws</Title>
-        <NewsList
-          NewsCardImage={NewsCardImage}
-          shownNewsItems={getNewsItems()}
-          onClick={goToNews}
-        />
-      </Container>
+      <NewsPage
+        newsItems={getNewsItems()}
+        Image={GatsbyImage}
+        fallBackLogo={getConstrainedLogoData()}
+      />
     </Layout>
   )
 }
