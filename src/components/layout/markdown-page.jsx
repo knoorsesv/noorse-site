9528193f0762e284e7ea530b5a216967199dc295
gvdp <vdputteglenn@gmail.com>
2023-09-22 21:22:18 +0200

    refactor: give markdownpage the option to have attachments

--- src/components/layout/markdown-page.jsx
+++ src/components/layout/markdown-page.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { MarkDown, Title } from '../index'
+import { Attachments, MarkDown, Title } from '../index'
 import { Container } from './index'
 
-export const MarkDownPage = ({ children, title }) => {
+export const MarkDownPage = ({ children, title, description, attachment }) => {
   return (
     <>
       <Helmet>
         <title>{title}</title>
+        <meta property="og:title" content={title} />
+        {description ? (
+          <meta property="og:description" content={description} />
+        ) : (
+          <></>
+        )}
       </Helmet>
       <Container>
         <Title>{title}</Title>
-        <MarkDown>{children}</MarkDown>
+        {children ? (
+          <MarkDown>{children}</MarkDown>
+        ) : (
+          <section>No content</section>
+        )}
+        {attachment ? (
+          <section className={'prose'}>
+            <Attachments attachments={attachment} />
+          </section>
+        ) : (
+          <></>
+        )}
       </Container>
     </>
   )
