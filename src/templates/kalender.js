a2bf72b5c770c6b379ae8085b2800808e1985018
gvdp <vdputteglenn@gmail.com>
2023-09-30 01:08:49 +0200

    chore: remove unused code

--- src/templates/kalender.js
+++ src/templates/kalender.js
@@ -1,40 +1,8 @@
-import { graphql } from 'gatsby'
 import React from 'react'
-import Layout from '../layouts/layout'
 import { CalendarPage } from '../components/pages'
-
-// export const query = graphql`
-//   query ($clubId: ID!, $startDate: String!, $endDate: String!) {
-//     vv {
-//       clubMatchesAssignations(
-//         clubId: $clubId
-//         language: nl
-//         startDate: $startDate
-//         endDate: $endDate
-//         hasLocation: true
-//       ) {
-//         id
-//         startTime
-//         title
-//         homeTeam {
-//           name
-//         }
-//         awayTeam {
-//           name
-//         }
-//         outcome {
-//           status
-//           homeTeamGoals
-//           awayTeamGoals
-//         }
-//       }
-//     }
-//   }
-// `
-// todo: outcome not needed in query?
+import Layout from '../layouts/layout'
 
 const KalenderTemplate = ({ pageContext: { clubMatchesAssignations } }) => {
-  console.log('clubMatchesAssignations', clubMatchesAssignations)
   return (
     <Layout>
       <CalendarPage games={clubMatchesAssignations || []} />
--- src/templates/team-page-template.js
+++ src/templates/team-page-template.js
@@ -1,45 +1,6 @@
-import { graphql } from 'gatsby'
 import React from 'react'
 import { TeamPage } from '../components/pages'
 import Layout from '../layouts/layout'
-
-// export const query = graphql`
-//   query ($teamId: ID!) {
-//     vv {
-//       # teamCalendar(teamId: $teamId, language: nl) {
-//       #   id
-//       #   startTime
-//       #   homeTeam {
-//       #     name
-//       #   }
-//       #   awayTeam {
-//       #     name
-//       #   }
-//       #   outcome {
-//       #     status
-//       #     homeTeamGoals
-//       #     awayTeamGoals
-//       #   }
-//       # }
-//       # teamSeriesAndRankings(teamId: $teamId, language: nl) {
-//       #   series {
-//       #     serieId
-//       #     name
-//       #   }
-//       #   rankings {
-//       #     name
-//       #     rankings {
-//       #       teams {
-//       #         name
-//       #         position
-//       #         points
-//       #       }
-//       #     }
-//       #   }
-//       # }
-//     }
-//   }
-// `
 
 const TeamPageTemplate = ({
   pageContext: {
