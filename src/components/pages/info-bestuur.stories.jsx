e14b3eb20ee8fc696ec9ea7910fd0326a2744e0d
gvdp <vdputteglenn@gmail.com>
2023-09-30 13:27:33 +0200

    refactor: map to nodes in query part of bestuurpage

--- src/components/pages/info-bestuur.stories.jsx
+++ src/components/pages/info-bestuur.stories.jsx
@@ -10,28 +10,22 @@ export default {
     leden: [
       {
         // todo: node should not be a part of this type
-        node: {
-          type: 'bestuursorgaan',
-          title: 'Voorzitter',
-          naam: 'Joske',
-          email: 'voorzitter@neurse',
-        },
+        type: 'bestuursorgaan',
+        title: 'Voorzitter',
+        naam: 'Joske',
+        email: 'voorzitter@neurse',
       },
       {
-        node: {
-          type: 'bestuursorgaan',
-          title: 'Iemand ans',
-          naam: 'Jefke',
-          email: 'voorzitter@neurse',
-        },
+        type: 'bestuursorgaan',
+        title: 'Iemand ans',
+        naam: 'Jefke',
+        email: 'voorzitter@neurse',
       },
       {
-        node: {
-          type: 'deelwerking',
-          title: 'Nog een deel',
-          naam: 'Gerard',
-          email: 'voorzitter@neurse',
-        },
+        type: 'deelwerking',
+        title: 'Nog een deel',
+        naam: 'Gerard',
+        email: 'voorzitter@neurse',
       },
     ],
   },
