7a9aa46f79370b3d9cd24baae32f16b474ff566b
gvdp <vdputteglenn@gmail.com>
2024-08-29 19:09:13 +0200

    feat: add division between bouwen in category navigation

--- src/react/team-navigation.jsx
+++ src/react/team-navigation.jsx
@@ -6,31 +6,82 @@ export const CategoryTeamNavigation = ({ category, header }) => {
     return <></>
   }
 
+  console.log(category.ploeg)
+
+  const bouwen = category.ploeg.reduce((bouwlijst, ploeg) => {
+    return {
+      ...bouwlijst,
+      [`${ploeg.bouw}`]: bouwlijst[ploeg.bouw]
+        ? [...bouwlijst[ploeg.bouw], ploeg]
+        : [ploeg],
+    }
+  }, {})
+
   return (
     <nav
       aria-labelledby="team-navigation"
-      className={`bg-green-light bg-opacity-25 p-4`}
+      className={`bg-green-light bg-opacity-25 p-6 flex flex-col items-center gap-8`}
     >
       <h3
         id="team-navigation"
-        className={'w-full border-b-2 border-black pb-2 text-center'}
+        className={'w-full border-b-2 border-black pb-2 text-center mb-2'}
       >
         {header || category.naam}
       </h3>
-      <div className={'flex-start mx-1 flex flex-wrap'}>
-        {category.ploeg &&
-          category.ploeg
-            .sort((ploeg1, ploeg2) => (ploeg1.naam > ploeg2.naam ? '1' : '-1'))
-            .map((ploeg) => (
-              <LinkWrapper
-                key={ploeg.naam}
-                href={`/team/${category.naam.toLowerCase()}/${ploeg.naam.toLowerCase()}`}
-                className={'mx-3 w-[80px] text-gray-dark underline'}
-              >
-                {ploeg.naam}
-              </LinkWrapper>
-            ))}
-      </div>
+      {Object.keys(bouwen)
+        .sort((bouw) => {
+          if (bouw === 'competitief') return -1
+          if (bouw === 'onderbouw') return -1
+          if (bouw === 'middenbouw') return -1
+        })
+        .map((bouw) => {
+          return (
+            <div className="flex flex-col items-center" key={bouw}>
+              <h4 className="capitalize font-bold mb-2">{bouw}</h4>
+              <div className={'w-full gap-2 flex-start flex flex-wrap'}>
+                {bouwen[bouw]
+                  .sort((ploeg1, ploeg2) => {
+                    const normalizedName1 = ploeg1.naam
+                      .replaceAll('Geel', '')
+                      .replaceAll('Groen', '')
+                      .replace(/^U/, '')
+                    const normalizedName2 = ploeg2.naam
+                      .replaceAll('Geel', '')
+                      .replaceAll('Groen', '')
+                      .replace(/^U/, '')
+                    if (
+                      !isNaN(parseInt(normalizedName1)) &&
+                      !isNaN(parseInt(normalizedName2))
+                    ) {
+                      if (
+                        parseInt(normalizedName1) === parseInt(normalizedName2)
+                      ) {
+                        if (ploeg1.naam.includes('Geel')) {
+                          return -1
+                        }
+                      }
+                      return (
+                        parseInt(normalizedName1) - parseInt(normalizedName2)
+                      )
+                    }
+
+                    return ploeg1.naam > ploeg2.naam ? '1' : '-1'
+                  })
+                  .map((ploeg) => (
+                    <LinkWrapper
+                      key={ploeg.naam}
+                      href={`/team/${category.naam.toLowerCase()}/${ploeg.naam.toLowerCase()}`}
+                      className={
+                        'text-center w-[80px] text-gray-dark underline'
+                      }
+                    >
+                      {ploeg.naam}
+                    </LinkWrapper>
+                  ))}
+              </div>
+            </div>
+          )
+        })}
     </nav>
   )
 }
