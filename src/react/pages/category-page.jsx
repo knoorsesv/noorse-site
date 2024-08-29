de2f442383358f8949e0fed13a7155d57e84ea3b
gvdp <vdputteglenn@gmail.com>
2024-08-29 19:26:08 +0200

    feat: group jeugd on 1 category page

--- src/react/pages/category-page.jsx
+++ src/react/pages/category-page.jsx
@@ -3,8 +3,36 @@ import { CategoryTeamNavigation, MarkDown, Section, SubTitle } from '../index'
 import { Page } from './page.jsx'
 
 export const CategoryPage = ({ category }) => {
+  if (category.categories?.length) {
+    return (
+      <Page title={category.naam}>
+        {category.categories.map((subCategory) => {
+          return (
+            <section
+              key={subCategory.props.categoryData.naam}
+              className="flex flex-col w-full items-center mb-10"
+            >
+              <h3 className="uppercase font-bold text-xl">
+                {subCategory.props.categoryData.naam}
+              </h3>
+              <CategoryContent category={subCategory.props.categoryData} />
+            </section>
+          )
+        })}
+      </Page>
+    )
+  }
+
   return (
     <Page title={category.naam}>
+      <CategoryContent category={category} />
+    </Page>
+  )
+}
+
+const CategoryContent = ({ category }) => {
+  return (
+    <>
       {category?.general_info && (
         <>
           <SubTitle>Info</SubTitle>
@@ -36,6 +64,6 @@ export const CategoryPage = ({ category }) => {
       ) : (
         <></>
       )}
-    </Page>
+    </>
   )
 }
