a5ea2759a5fc4fada8ab6071e9ef6b95c572ac08
gvdp <vdputteglenn@gmail.com>
2024-10-01 20:26:13 +0200

    feat: update navigation structure

--- src/env/constants.js
+++ src/env/constants.js
@@ -1,4 +1,5 @@
 export const webshopLink = 'https://noorsesv.shop4clubs.eu/'
+export const trooperLink = 'https://www.trooper.be/noorse'
 export const newsletterLink = 'http://eepurl.com/hRibPn'
 
 // todo: these category names could be fetched from contentful
@@ -21,41 +22,42 @@ export const siteMap = {
           link: '/info/fairplay',
         },
         {
-          name: 'Kalender',
-          link: '/info/kalender',
-        },
-        {
-          name: 'Lidmaatschap',
-          link: '/info/lidmaatschap',
-        },
-        {
-          name: 'Nieuws',
-          link: '/info/nieuws',
-        },
-        {
-          name: 'Sponsors',
-          link: '/info/sponsors',
-        },
-        {
           name: 'Structuur',
           link: '/info/structuur',
         },
-        {
-          name: 'Verzekering',
-          link: '/info/verzekering',
-        },
-        {
-          name: 'Webshop',
-          extLink: webshopLink,
-        },
       ],
     },
-    { name: 'Senioren', link: '/senioren' },
+    {
+      name: 'Kalender',
+      link: '/info/kalender',
+    },
+    {
+      name: 'Lidmaatschap',
+      link: '/info/lidmaatschap',
+    },
+    {
+      name: 'Nieuws',
+      link: '/info/nieuws',
+    },
+    {
+      name: 'Sponsors',
+      link: '/info/sponsors',
+    },
+    {
+      name: 'Verzekering',
+      link: '/info/verzekering',
+    },
+    {
+      name: 'Trooper',
+      extLink: trooperLink,
+    },
+    {
+      name: 'Webshop',
+      extLink: webshopLink,
+    },
+    { name: 'Heren', link: '/senioren' },
     { name: 'Jeugd', link: '/jeugd' },
-    // { name: 'Jeugd Jongens', link: '/jeugd jongens' },
     { name: 'Dames', link: '/dames' },
-    // { name: 'Jeugd Meisjes', link: '/jeugd meisjes' },
-    // { name: 'G-Werking', link: '/g-werking' },
     { name: 'Contact', link: '/contact' },
   ],
 }
