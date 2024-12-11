6ae16eab247c0fba83ae43447ea7f8f104d2e234
gvdp <vdputteglenn@gmail.com>
2024-12-11 23:11:45 +0100

    test: dont override global property in navbar story

--- src/react/navbar.stories.jsx
+++ src/react/navbar.stories.jsx
@@ -26,10 +26,11 @@ export const OpenedMenu = Template.bind({})
 // OpenedMenu.parameters = {
 //   autoplay: true
 // }
-// OpenedMenu.globals = {
-//   // 👇 Override default viewport for this story
-//   viewport: { value: 'medium' },
-// }
+OpenedMenu.parameters = {
+  viewport: {
+    defaultViewport: 'medium',
+  },
+}
 OpenedMenu.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   await userEvent.click(canvas.getByLabelText('Open menu'))
