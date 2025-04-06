6d279963c76a01453710fae22596d6ddb1f7087d
gvdp <vdputteglenn@gmail.com>
2025-04-06 11:29:30 +0200

    chore: make storybook config ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,19 @@
-module.exports = {
-  stories: ['../src/react/**/*.stories.@(js|jsx|ts|tsx)'],
+import type { StorybookConfig } from '@storybook/react-vite'
 
+const confg: StorybookConfig = {
+  stories: ['../src/react/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
   ],
-
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
-
+  framework: '@storybook/react-vite',
   staticDirs: ['../public/images'],
-
   docs: {},
-
   typescript: {
     reactDocgen: 'react-docgen-typescript',
   },
 }
+
+export default confg
