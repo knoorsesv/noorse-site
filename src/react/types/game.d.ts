89d57811ce20e2b0b73db47b184b0335a616ca25
gvdp <vdputteglenn@gmail.com>
2025-04-06 11:54:30 +0200

    refactor: split out type definitions

---
+++ src/react/types/game.d.ts
@@ -0,0 +1,14 @@
+import type { Team } from './team'
+
+export interface Game {
+  startTime: string
+  startDate: string
+  id: string
+  homeTeam: Team
+  awayTeam: Team
+  outcome: {
+    homeTeamGoals: number
+    awayTeamGoals: number
+    status: string
+  }
+}
