0838c25c0c2f7d9b21620cdfab4eef3490e0dc32
gvdp <vdputteglenn@gmail.com>
2025-04-19 01:35:53 +0200

    refactor: type team page

--- src/react/types/team.d.ts
+++ src/react/types/team.d.ts
@@ -1,11 +1,22 @@
+import type { Category } from './category'
+
+// todo: fix all these undefined and null types
 export interface Team {
   name?: string
   bouw?: string
   naam?: string
   position?: number
-  points?: number
+  points?: number | undefined
   coach?: string[]
   afgevaardigde?: string[]
   training?: string[]
   categorie?: Category
+  naamOpVoetbalVlaanderen?: string
+  ploegfoto?: {
+    fields: {
+      file: {
+        url: string
+      }
+    }
+  }
 }
