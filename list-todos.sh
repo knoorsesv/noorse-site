f1e82e25d86ed314c8e6d423171f81776124557f
gvdp <vdputteglenn@gmail.com>
2025-04-18 20:25:56 +0200

    chore: add listing todos to pre commit hook

--- list-todos.sh
+++ list-todos.sh
@@ -3,3 +3,5 @@
 rg -i todo --vimgrep | sd '(^.*):(\d+):\d+:\s*(.*$)' ' - [ ] [$1](https://github.com/knoorsesv/noorse-site/blob/main/$1#L$2)' > .todos
 cat .todos
 gh issue edit 488 --body-file .todos
+
+git add .todos
