f87a63dea0c48d1649ef51adb90c872ef670270e
gvdp <vdputteglenn@gmail.com>
2025-04-19 19:12:47 +0200

    refactor: type Page

--- src/react/pages/page.tsx
+++ src/react/pages/page.tsx
@@ -1,8 +1,11 @@
 import { Helmet } from 'react-helmet'
 import { Title } from '../index'
 import { Container } from '../layout'
+import type { FC, PropsWithChildren } from 'react'
 
-export const Page = ({ title, children, centered = true }) => {
+export const Page: FC<
+  PropsWithChildren<{ title: string; centered?: boolean }>
+> = ({ title, children, centered = true }) => {
   return (
     <>
       <Helmet>
