1d708a59aa19159d725d41cb66e63d62a32b88ec
gvdp <vdputteglenn@gmail.com>
2026-01-13 20:39:35 +0100

    chore: remove listing typescript conversions as todos, only remaining files are config

--- list-todos.sh
+++ list-todos.sh
@@ -2,8 +2,6 @@
 
 rg -i todo --vimgrep | sd '(^.*):(\d+):\d+:\s*(.*$)' ' - [ ] Todo in: [$1](https://github.com/knoorsesv/noorse-site/blob/main/$1#L$2)' > .todos.md
 
-fd -e js -e jsx | sd '(.*)' ' - [ ] Convert [$1](https://github.com/knoorsesv/noorse-site/blob/main/$1) to typescript' >> .todos.md
-
 cat .todos.md
 gh issue edit 488 --body-file .todos.md
 
