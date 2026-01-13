b0adc92f990e8f89b7bcb0c33e804e08bab4f04d
gvdp <vdputteglenn@gmail.com>
2026-01-13 20:42:06 +0100

    ci: use github action instead of custom scripts for creating issues out of todos

--- list-todos.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/zsh
-
-rg -i todo --vimgrep | sd '(^.*):(\d+):\d+:\s*(.*$)' ' - [ ] Todo in: [$1](https://github.com/knoorsesv/noorse-site/blob/main/$1#L$2)' > .todos.md
-
-cat .todos.md
-gh issue edit 488 --body-file .todos.md
-
-git add .todos.md
